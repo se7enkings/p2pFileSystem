1c42236709cbd7e615be3a65b5bb6720eea8256f
CRVV <crvv.mail@gmail.com>
2015-03-19 00:48:47 +0800

    201503190048

--- transfer/neighborDiscovery.go
+++ transfer/neighborDiscovery.go
@@ -3,23 +3,25 @@ package transfer
 import (
 	"encoding/binary"
 	"encoding/json"
-	"fmt"
 	"github.com/CRVV/p2pFileSystem/filesystem"
 	"github.com/CRVV/p2pFileSystem/settings"
 	"net"
 	"time"
-    "os"
 )
 
 func SendNeighborSolicitation(targetAddr string) {
-	conn, err := net.Dial("udp", targetAddr+settings.NeighborDiscoveryPort)
+	udpAddr, err := net.ResolveUDPAddr("udp", targetAddr+settings.NeighborDiscoveryPort)
+	if err != nil {
+		return
+	}
+	conn, err := net.DialUDP("udp", nil, udpAddr)
 	if err != nil {
 		return
 	}
 	defer conn.Close()
 
-	conn.Write([]byte(settings.NeighborDiscoveryProtocol))
-	buff := make([]byte, settings.MessageHeaderSize)
+	var buff []byte
+	buff = append(buff, []byte(settings.NeighborDiscoveryProtocol)...)
 
 	addr, _, err := net.SplitHostPort(conn.LocalAddr().String())
 	if err != nil {
@@ -34,12 +36,16 @@ func SendNeighborSolicitation(targetAddr string) {
 	if err != nil {
 		return
 	}
-	binary.LittleEndian.PutUint32(buff, uint32(len(message)))
-	conn.Write(buff)
-	conn.Write(message)
+	size := make([]byte, settings.MessageHeaderSize)
+	binary.LittleEndian.PutUint32(size, uint32(len(message)))
+	buff = append(buff, size...)
+	buff = append(buff, message...)
+	_, err = conn.Write(buff)
+	if err != nil {
+		panic(err)
+	}
 }
 func StartNeighborDiscoveryServer() {
-	fmt.Println("start nd server")
 	conn, err := net.ListenUDP("udp", &net.UDPAddr{
 		IP:   net.IPv4zero,
 		Port: 1540})
@@ -49,35 +55,31 @@ func StartNeighborDiscoveryServer() {
 	defer conn.Close()
 	for {
 		buff := make([]byte, settings.NeighborDiscoveryMessageBufferSize)
-        _, _, err := conn.ReadFromUDP(buff)
-        if err != nil {
-            continue
-        }
-        os.Stdout.Write(buff)
-        headerSize := settings.MessageHeaderSize
+		_, _, err := conn.ReadFromUDP(buff)
+		if err != nil {
+			continue
+		}
+		headerSize := settings.MessageHeaderSize
 		messageType := string(buff[0:headerSize])
-        fmt.Printf("\n\n%s\n", messageType)
-//		size := binary.LittleEndian.Uint32(buff[headerSize:headerSize*2])
+		if messageType != settings.NeighborDiscoveryProtocol {
+			continue
+		}
+		size := binary.LittleEndian.Uint32(buff[headerSize : headerSize*2])
 		if messageType == settings.NeighborDiscoveryProtocol {
-			client, err := Json2ClientMessage(buff[headerSize*2:])
+			client, err := Json2ClientMessage(buff[headerSize*2 : headerSize*2+int(size)])
 			if err != nil {
 				continue
 			}
-			fmt.Println(client)
 			onReceiveNeighborSolicitation(client)
 		}
 	}
 }
 func onReceiveNeighborSolicitation(client NDMessage) {
-	fmt.Println("receive")
 	if client.Group == settings.GetSettings().GetGroupName() && client.ID != filesystem.ID {
-		fmt.Println("same group, different id")
 		_, ok := filesystem.Clients[client.Username]
 		if ok || client.Username == settings.GetSettings().GetUsername() {
-			fmt.Println("send invalid username")
 			SendMessage(client.Addr, settings.InvalidUsername, []byte(settings.InvalidUsername))
 		} else {
-			fmt.Println("send ndp to received client")
 			SendNeighborSolicitation(client.Addr)
 			filesystem.OnDiscoverClient(client.Username, client.Addr)
 		}
@@ -85,7 +87,7 @@ func onReceiveNeighborSolicitation(client NDMessage) {
 }
 func InitNeighborDiscovery() {
 	go StartNeighborDiscoveryServer()
-	timer := time.Tick(time.Second * 5)
+	timer := time.Tick(time.Second * 1)
 	for {
 		SendNeighborSolicitation(settings.BroadcastAddress)
 		<-timer
