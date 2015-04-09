03f87f8ab67812e908fe1c1c34a8f27ac95fc727
CRVV <crvv.pku@gmail.com>
2015-04-09 11:04:37 +0800

    simplify build script and ignore file generated by build script

--- build.sh
+++ build.sh
@@ -1,15 +1,4 @@
-# Save the pwd before we run anything
-PRE_PWD=`pwd`
- 
-# Determine the build script's actual directory, following symlinks
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
-BUILD_DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
- 
-# Derive the project name from the directory
-PROJECT="$(basename $BUILD_DIR)"
 
-cd $BUILD_DIR
 cd data
 
 # Prepare files
@@ -21,24 +10,17 @@ else
     $GOPATH/bin/go-bindata -pkg="data" -o data.go -ignore=\.\*\\.go\|\.\*\\.coffee  ./...
 fi
 
-cd $BUILD_DIR
+cd ..
 
-# Build the project
-mkdir -p bin
 go build
  
 EXIT_STATUS=$?
  
-if [ $EXIT_STATUS == 0 ]; then
+if [ "$EXIT_STATUS" = "0" ]; then
   echo "Build succeeded"
 else
   echo "Build failed"
 fi
  
-# Change back to where we were
-cd $PRE_PWD
- 
 exit $EXIT_STATUS
-
-
 
