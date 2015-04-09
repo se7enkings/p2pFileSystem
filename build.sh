748c842bf1d29bd4c9a8b6ce08408544b6fb9d41
CRVV <crvv.pku@gmail.com>
2015-04-09 15:41:20 +0800

    improve build script

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 
-cd data
+PROJECT="$(basename `pwd`)"
+
+checkErr () {
+    EXIT_STATUS=$1
+    if [ "$EXIT_STATUS" != "0" ]; then
+        echo "Build failed"
+        exit $EXIT_STATUS
+    fi
+}
 
 # Prepare files
+cd data
 if [ "$1" = "debug" ]; then
     coffee -b -c -m js/*.coffee
     $GOPATH/bin/go-bindata -debug -pkg="data" -o data.go -ignore=\.\*\\.go  ./...
@@ -9,18 +18,32 @@ else
     coffee -b -c js/*.coffee
     $GOPATH/bin/go-bindata -pkg="data" -o data.go -ignore=\.\*\\.go\|\.\*\\.coffee  ./...
 fi
-
 cd ..
 
-go build
- 
-EXIT_STATUS=$?
- 
-if [ "$EXIT_STATUS" = "0" ]; then
-  echo "Build succeeded"
-else
-  echo "Build failed"
+if [ "$1" = "release" ]; then
+    oss="windows darwin linux"
+    archs="amd64 386"
+    for os in $oss
+    do
+        for arch in $archs
+        do
+            export GOOS=$os
+            export GOARCH=$arch
+            filename=${PROJECT}_${os}_${arch}
+            if [ "$os" = "windows" ]; then
+                filename=$filename.exe
+            fi
+            go build -o $filename
+            checkErr $?
+        done
+    done
 fi
+
+unset GOARCH
+unset GOOS
+go build
+checkErr $?
  
-exit $EXIT_STATUS
+echo "Build succeeded"
+exit 0
 
